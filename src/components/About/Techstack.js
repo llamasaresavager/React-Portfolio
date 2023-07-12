c39aa110c24003456a83f58942e4de71a24463f1
trv893 <trevor@pertainium.com>
2023-07-12 12:41:55 -0400

    update techstack

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -15,37 +15,37 @@ import {
   SiMicrosoftsqlserver
 } from "react-icons/si";
 
-
+import { TbBrandNextjs, TbBrandReactNative, TbBrandTypescript, TbBrandCSharp } from "react-icons/tb";
 
 function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
-        <CgCPlusPlus />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
         <DiJavascript1 />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiNodejs />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiReact />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiMongodb />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiGit />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiPython />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <FaRaspberryPi />
+        <TbBrandCSharp />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <SiArduino />
+        <DiReact />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <TbBrandReactNative />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <TbBrandNextjs />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiNodejs />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiMongodb />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <TbBrandTypescript />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiMicrosoftsqlserver />
